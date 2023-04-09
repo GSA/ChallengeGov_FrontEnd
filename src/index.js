ccd8c5be2824d28c64b0771fe170e8033b30cb2c
jdonis <alejandro.donis@gmail.com>
2023-04-09 10:34:15 -0400

    change BrowseRouter to HasRourter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import queryString from 'query-string'
-import { BrowserRouter, Redirect, Route, Router, Switch, useLocation } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, HasRouter, Router, Switch, useLocation } from "react-router-dom";
 import { IndexRoutes } from "./routes/index";
 import * as serviceWorker from './serviceWorker';
 import { useTracking } from './useTracking'
@@ -57,12 +57,12 @@ const renderRouter = () => (
   }}>
     {/* <BrowserRouter>   */}
 
-   <BrowserRouter basename='/challenge-react'>
+   <HasRouter basename='/challenge-react'>
     <Switch>
       <Route exact path="/" component={LandingPage} />
       <Route path="/chal/:id"  render={({ match }) => <DetailsPage challengeId={match.params.id} />} />
     </Switch>
-   </BrowserRouter>
+   </HasRouter>
 
       {/* <Application />
     </BrowserRouter> */}
